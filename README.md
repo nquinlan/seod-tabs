8537112df1aad2c10dcf29bed28c93bc990dd4db
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2013-10-17 11:48:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SEO'd Tabs
+Google is just starting to accept dynamic content. However, there are a number of problems with serving content exclusively through AJAX.
+
+To the best of my knowledge this sample code will split the difference, serving content dynamically while still allowing Google to index the page (and allow users without Javascript enabled to browse your site).
+
+Although I use tabs here, there's no reason it won't work using a different navigation strategy.
+
+Some examples of websites who have used a similar strategy are:
+
+- [Twitter.com](http://twitter.com)
---
+++ js/tabs.js
@@ -0,0 +1,29 @@
+$(function () {
+
+	$(".nav-tabs a").click(function (e) {
+		// Prevent the default behavior
+		e.preventDefault();
+
+		// Get the new content
+		var contentName = $(this).attr("id");
+		var contentDestination = $(this).attr("href");
+		var $target = $(this);
+		$.get("content/" + contentName + ".json", null, function (content) {
+			console.log($target, contentName, contentDestination);
+
+			// Change the content
+			$("#content").html(content.content);
+
+			// Change the tabs
+			// This isn't required, but it's a nice touch
+			$(".nav-tabs li").removeClass("active");
+			$target.parent().addClass("active");
+
+			// Change the apparent URL
+			// Again, this isn't required, but it's a nice touch
+			history.pushState({ "content" : content.content}, contentName, contentDestination);
+		});
+
+	});
+
+});
